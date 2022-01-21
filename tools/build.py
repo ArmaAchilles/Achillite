f23f014a922b4dfc5ab9f44fc08df3ff0194c871
Kex <kexanone@gmail.com>
2022-01-21 19:48:21 +0100

    Add validator to builder

--- tools/build.py
+++ tools/build.py
@@ -4,7 +4,9 @@ import json
 import re
 from string import Template
 import subprocess
+from sqf_validator import main as validator
 
+os.chdir(Path(__file__).parent)
 with open('../meta.json', 'r') as stream:
     meta_data = json.load(stream)
 with open('../meta.json', 'r') as stream:
@@ -55,6 +57,13 @@ def build_composition(init_body):
         stream.write(body)
 
 if __name__ == '__main__':
+    if validator():
+        raise RuntimeError('SQF Validation FAILED')
+    print()
+    print('PROCESSING COMPONENTS')
+    print('---------------------')
+    print()
+    i_fn = 0
     body = ''
     for component_dir in src_root.iterdir():
         if component_dir.is_file():
@@ -68,4 +77,11 @@ if __name__ == '__main__':
                 fn_var = f'{meta_data["MOD_PREFIX"]}_{component_name}_fnc_{fn_name}'
             content = minimize(preprocess(fn_file))
             body += f'{fn_var}={content};'
+            i_fn += 1
+    print(f'Processed {i_fn} functions, {len(body.encode("utf8")) / 1e3:.1f} KB in total.')
+    print()
+    print('BUILDING COMPOSITION')
+    print('---------------------')
+    print()
     build_composition(body)
+    print(f'Build completed!')
